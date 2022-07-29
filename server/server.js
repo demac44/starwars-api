b241dd288aa777363ff12530770a1d4b3619c7b6
demac44 <demirumejr2@gmail.com>
2022-07-29 17:38:30 +0300

    Project init

---
+++ server/server.js
@@ -0,0 +1,33 @@
+// import { config } from "dotenv"
+
+// if (process.env.NODE_ENV !== 'production') {
+//     config()
+//   }
+
+
+import express from 'express'
+import { join, resolve } from 'path'
+import cors from "cors"
+
+const app = express()
+
+const __dirname = resolve()
+app.use(express.static(join(__dirname, "client", "build")))
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET","PUT","POST","DELETE","UPDATE","OPTIONS"]
+}))
+
+
+app.get("*", (req, res) => {
+    res.sendFile(join(__dirname, "client", "build", "index.html"))
+})    
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Server started!'))
